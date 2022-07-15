cfb730ea65e6d244c99ae56be9b4e0cb2a757ff2
taty2010 <shydalys123@gmail.com>
2022-07-15 14:44:00 -0500

    revert back to orig req

--- netlify/functions/gridImages.js
+++ netlify/functions/gridImages.js
@@ -5,44 +5,24 @@ import fetch from 'node-fetch';
 export const handler = async () => {
   const baseUrl = 'https://api.pexels.com/v1/collections/gb7kvpf'
 
-  try {
     const response = await fetch(baseUrl, {
       method: 'GET',
       headers: {
         Authorization: `${process.env.PEXELS_API_TOKEN}`
       }
-    });
-    const data = await response.json();
-    const images = await data.media.map((img) => {
-      return img.src.original
     })
-    return { statusCode: 200, body: JSON.stringify({ images }) };
-  } catch (error) {
-    console.log(error);
+    .then((res) => res.json())
+    .catch((err) => console.error(err))
+
+    const images = response.media.map((img) => {
+        return img.src.original
+    })
+
     return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed fetching data' }),
-    };
-  }
-
-    // const response = await fetch(baseUrl, {
-    //   method: 'GET',
-    //   headers: {
-    //     Authorization: `${process.env.PEXELS_API_TOKEN}`
-    //   }
-    // })
-    // .then((res) => res.json())
-    // .catch((err) => console.error(err))
-
-    // const images = response.media.map((img) => {
-    //     return img.src.original
-    // })
-
-    // return {
-    //   statusCode: 200,
-    //   headers: {
-    //     'Content-Type': 'text/html'
-    //   },
-    //   body: JSON.stringify(images)
-    // }
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html'
+      },
+      body: JSON.stringify(images)
+    }
 }
