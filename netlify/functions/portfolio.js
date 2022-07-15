b325e1f5b2e751a689059220663d9e938d1e5ade
taty2010 <shydalys123@gmail.com>
2022-07-15 14:23:14 -0500

    testing function only

--- netlify/functions/portfolio.js
+++ netlify/functions/portfolio.js
@@ -1,19 +1,47 @@
-data = require('./data.json');
+import fetch from 'node-fetch';
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async () => {
-  try {
-    // console.log(data)
-    const randomIndex = Math.floor(Math.random() * data.length)
-    const randomImage = data[randomIndex].img
+
+export const handler = async () => {
+  const baseUrl = 'https://api.pexels.com/v1/collections/x043eny'
+
+    const response = await fetch(baseUrl, {
+      method: 'GET',
+      headers: {
+        Authorization: `${process.env.PEXELS_API_TOKEN}`
+      }
+    })
+    .then((res) => res.json())
+    .catch((err) => err)
+
+    const images = response.media.map((img) => {
+        return img.src.original
+    })
+    const randomIndex = Math.floor(Math.random() * images.length)
+    const randomImage = images[randomIndex]
     return {
       statusCode: 200,
-      body: randomImage,
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: randomImage
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
 }
+
+
+// data = require('./data.json');
+
+// // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+// exports.handler = async () => {
+//   try {
+//     // console.log(data)
+//     const randomIndex = Math.floor(Math.random() * data.length)
+//     const randomImage = data[randomIndex].img
+//     return {
+//       statusCode: 200,
+//       body: randomImage,
+//       // // more keys you can return:
+//       // headers: { "headerName": "headerValue", ... },
+//       // isBase64Encoded: true,
+//     }
+//   } catch (error) {
+//     return { statusCode: 500, body: error.toString() }
+//   }
+// }
